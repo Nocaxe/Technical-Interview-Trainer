1c5e25c71d8678ef2bc710163e66f68159471729
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-05-26 21:39:01 +0800

    Create placeholder interview and debrief screens

---
+++ frontend/src/screens/DebriefScreen.jsx
@@ -0,0 +1,3 @@
+export default function DebriefScreen() {
+  return <div>Debrief screen coming soon.</div>;
+}
---
+++ frontend/src/screens/InterviewScreen.jsx
@@ -0,0 +1,3 @@
+export default function InterviewScreen({ problem }) {
+  return <div>Interview screen coming soon. Problem: {problem.title}</div>;
+}
